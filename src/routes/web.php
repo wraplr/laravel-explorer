9cc7cb0de05b9c60c130a5f902135f0cdb8d1da5
wraplr <wraplr@outlook.com>
2020-03-17 15:54:43 +0100

    copy, cut and paste implemented

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,23 +1,28 @@
 <?php
 
 Route::group(['prefix' => config('wlrle.url_prefix'), 'middleware' => config('wlrle.middleware'), 'namespace' => '\WrapLr\LaravelExplorer\App\Http\Controllers'], function () {
-    Route::get('/', 'ExplorerController@show')->name('wraplr.laravel-explorer.show');
-    Route::get('/refresh', 'ExplorerController@refresh')->name('wraplr.laravel-explorer.refresh');
+    Route::get('/', 'ExplorerController@show');
+    Route::get('/refresh', 'ExplorerController@refresh');
 
     Route::group(['prefix' => 'directory'], function () {
-        Route::get('/{id}/change/{request}', 'DirectoryController@change')->name('wraplr.laravel-explorer.directory.change');
-        Route::post('/create', 'DirectoryController@create')->name('wraplr.laravel-explorer.directory.create');
-        Route::patch('/{id}/rename', 'DirectoryController@rename')->name('wraplr.laravel-explorer.directory.rename');
-        Route::delete('/delete', 'DirectoryController@delete')->name('wraplr.laravel-explorer.directory.delete');
+        Route::get('/{id}/change/{request}', 'DirectoryController@change');
+        Route::post('/create', 'DirectoryController@create');
+        Route::patch('/{id}/rename', 'DirectoryController@rename');
     });
 
     Route::group(['prefix' => 'file'], function () {
-        Route::post('/upload', 'FileController@upload')->name('wraplr.laravel-explorer.file.upload');
-        Route::patch('/{id}/rename', 'FileController@rename')->name('wraplr.laravel-explorer.file.rename');
-        Route::delete('/delete', 'FileController@delete')->name('wraplr.laravel-explorer.file.delete');
+        Route::post('/upload', 'FileController@upload');
+        Route::patch('/{id}/rename', 'FileController@rename');
+    });
+
+    Route::group(['prefix' => 'item'], function () {
+        Route::post('/copy', 'ItemController@copy');
+        Route::post('/cut', 'ItemController@cut');
+        Route::post('/paste', 'ItemController@paste');
+        Route::delete('/delete', 'ItemController@delete');
     });
 
     Route::group(['prefix' => 'view'], function () {
-        Route::get('/{view}/{file}', 'ViewController@create')->name('wraplr.laravel-explorer.view.create');
+        Route::get('/{view}/{file}', 'ViewController@create');
     });
 });
